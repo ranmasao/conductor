cefb5bde7d53a591ca10fa06312a6bfc502d5c76
Daniil Romanov <romanov.at.bg@gmail.com>
2026-09-12 18:00:53 +0300

    Remove retired application compatibility seam

--- src/conductor/application.py
+++
@@ -1,6 +0,0 @@
-"""Compatibility imports for the retired Application authority layer."""
-
-from conductor.service import ServiceEngine
-
-Application = ServiceEngine
-ConductorApplication = ServiceEngine
